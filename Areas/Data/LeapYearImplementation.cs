4bb4140bc367e9c8e801ff4512c34befdc0302db
Jula <loveliljan876@wp.pl>
2023-06-06 21:36:44 +0200

    Dodaj pliki projektów.

---
+++ Areas/Data/LeapYearImplementation.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PS6.Areas.Data.Models;
+using PS6.Areas.YearDb;
+using PS6.Pages.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS6.Areas.Data
+{
+    public class LeapYearImplementation : ILeapYearInterface
+    {
+        private readonly YearDbContext context;
+
+        public LeapYearImplementation(PS6.Areas.YearDb.YearDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<YearValidationResult>> GetResults(int pageIndex)
+        {
+            var results = await PaginatedList<YearValidationResult>.CreateAsync(
+            context.YearValidationResult.AsNoTracking(), pageIndex, 20);
+
+
+            return results;
+        }
+
+        public async Task Save(HttpContext httpContext, YearResponse response)
+        {
+            var userId = httpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userName = httpContext.User.Identity.Name;
+
+            var yearValidationResult = new YearValidationResult
+            {
+                Year = response.Year,
+                Result = response.ToString(),
+                TimeAdded = DateTime.Now,
+                UserId = userId,
+                UserLogin = userName
+            };
+
+            context.YearValidationResult.Add(yearValidationResult);
+            await context.SaveChangesAsync();
+        }
+
+        public Task Save(YearForm yearForm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pages/History.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PS6.Areas.Data;
+using PS6.Areas.Data.Models;
+using PS6.Areas.YearDb;
+
+namespace PS6.Pages.History
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILeapYearInterface leapYearService;
+
+        public IndexModel(ILeapYearInterface leapYearService)
+        {
+            this.leapYearService = leapYearService;
+        }
+
+        public IList<YearValidationResult> YearValidationResult { get;set; } = default!;
+
+        public async Task OnGetAsync(int pageIndex = 1)
+        {
+            YearValidationResult = await leapYearService.GetResults(pageIndex);
+        }
+    }
+}
